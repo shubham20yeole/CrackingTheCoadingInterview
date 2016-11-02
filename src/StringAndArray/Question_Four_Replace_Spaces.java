b98293bd300d39f559e32edd2193dea59a03b0de
Shubham Yeole <shubham20.yeole@gmail.com>
2016-11-01 22:18:35 -0400

    Update DataStructures Algorithms

---
+++ src/StringAndArray/Question_Four_Replace_Spaces.java
@@ -0,0 +1,40 @@
+//Write a method to replace all spaces in a string with '%20'. You may assume that the
+//string has sufficient space at the end of the string to hold the additional characters,
+//and that you are given the "true" length of the string. (Note: if implementing in Java,
+//please use a character array so that you can perform this operation in place.)
+
+package StringAndArray;
+
+public class Question_Four_Replace_Spaces {
+	public static void main(String[] args) {
+		String string = "Mr Shubham Dilip Yeole";
+		char[] array = string.toCharArray();
+		System.out.println("\n\nInput : " +string);
+		System.out.println("\n\nResult: "+method_1(array,string.length()));
+//		method_1(array,string.length());
+	}
+
+	private static String method_1(char[] array, int length) {
+		int spaceCount = 0;
+		for(int i=0; i<array.length; i++){
+			if(array[i]==' ') spaceCount++;
+		}
+		int count = 0;
+		int newLength = length + spaceCount*2;
+		char[] newArray = new char[newLength];
+
+		for(int i= 0; i<array.length; i++){
+			if(array[i]==' '){
+				newArray[count++] = '%';
+				newArray[count++] = '2';
+				newArray[count++] = '0';
+			}else{
+				newArray[count] = array[i];
+				count++;
+			}
+		}
+        String newString1 = new String(newArray);
+
+		return newString1;
+	}
+}
---
+++ src/StringAndArray/Question_Three_StringPermutation.java
@@ -0,0 +1,51 @@
+//1.3 Given two strings, write a method to decide if one is a permutation of the other
+package StringAndArray;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Question_Three_StringPermutation {
+	public static void main(String[] args) {
+		System.out.println("Purmutation Status: "+method_1("hello","dog"));
+		System.out.println("Purmutation Status: "+method_2("odg","dog"));
+	}
+
+	private static boolean method_1(String sample_1, String sample_2) {
+		char[] array1 = sample_1.toCharArray();
+		char[] array2 = sample_2.toCharArray();
+		Arrays.sort(array1);
+		Arrays.sort(array2);
+		sample_1 = String.valueOf(array1);
+		sample_2 = String.valueOf(array2);
+		
+		if(sample_1.equals(sample_2)) return true;
+		else return false;
+	}
+
+	private static boolean method_2(String sample_1, String sample_2) {
+		char[] array1 = sample_1.toCharArray();
+		char[] array2 = sample_2.toCharArray();
+		boolean result = false;
+		int[] abc = new int[256];
+		for(int i=0; i <array1.length; i++){
+			int val = array1[i];
+			abc[val]++;
+		}
+		for(int i=0; i <array2.length; i++){
+			int val2 = array2[i];
+			abc[val2]--;
+			if(abc[val2]<0) {
+				result = false;
+				break;
+			}else{
+				result = true;
+			}
+		}
+
+		return result;
+	}
+}
+
+// 1 2 3 4 5 6 7 
+// 1
+
